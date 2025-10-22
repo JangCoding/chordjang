a54a877487b165e7fe03e6856d7427d8b664fefa
CodingJang <jjangga813@naver.com>
2025-10-22 12:45:16 +0900

    init commit

---
+++ src/main/java/com/example/chordjang/ChordJangApplication.java
@@ -0,0 +1,13 @@
+package com.example.chordjang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChordJangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChordJangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chordjang/ChordJangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chordjang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChordJangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
