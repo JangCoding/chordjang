985f57a8e16726286c71ff9372a3337fff50e89d
jhjang <jhjang@naver.com>
2025-11-04 13:46:40 +0900

    chore : swagger 의존성 추가, config 정의

---
+++ src/main/java/com/example/chordjang/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.example.chordjang.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Course API")
+                        .description("Course API schema")
+                        .version("1.0.0"));
+    }
+}
