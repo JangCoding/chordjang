f8beded6c514fddbf57590bc12071022dcf66e6e
jhjang <jhjang@naver.com>
2025-11-04 16:34:28 +0900

    feat : user MVC 개발 일시중지 Todo : Auth 관련 개발 후 JWT 통해 데이터 가져오기, createUser 이동, password 변경 등 추가 필요

--- src/main/java/com/example/chordjang/user/DTO/UpdateUserRequestDTO.java
+++ src/main/java/com/example/chordjang/user/DTO/UpdateUserRequestDTO.java
@@ -2,18 +2,10 @@ package com.example.chordjang.user.DTO;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UpdateUserRequestDTO {
-    @NotBlank(message = "아이디는 필수 입력 값입니다!")
-    String userId;
-
-    @NotBlank(message = "비밀번호는 필수 입력 값입니다!")
-    @Size(min=8, message = "비밀번호는 8자 이상이어야 합니다!")
-    String password;
-
     @NotBlank(message = "이메일은 필수 입력 값입니다!")
     @Email(message = "이메일 형식이 올바르지 않습니다!")
     String email;
--- src/main/java/com/example/chordjang/user/UserServiceImpl.java
+++ src/main/java/com/example/chordjang/user/UserServiceImpl.java
@@ -6,21 +6,31 @@ import com.example.chordjang.user.DTO.UserResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
     @Override
     public UserResponseDTO createUser(CreateUserRequestDTO req) {
+
+        if(userRepository.findByUserId(req.getUserId()).isPresent())
+            throw new IllegalStateException("이미 사용중인 ID 입니다.");
+
+        if(userRepository.findByEmail(req.getEmail()).isPresent())
+            throw new IllegalStateException("이미 사용중인 Email 입니다.");
+
         User user = User.builder()
                 .userId(req.getUserId())
                 .email(req.getEmail())
                 .password(req.getPassword())
+                .role(RoleEnum.USER)
                 .build();
 
         return UserResponseDTO.fromEntity(userRepository.save(user));
@@ -29,23 +39,38 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserResponseDTO findUserBy(String userId, String email) {
         if(userId!=null)
-            return userRepository.findByUserId(userId).map(UserResponseDTO::fromEntity).orElse(null);
-        else if(email!=null)
-            return userRepository.findByEmail(email).map(UserResponseDTO::fromEntity).orElse(null);
-        else
-            throw new IllegalArgumentException("Unknown search type: ");
+            return userRepository.findByUserId(userId)
+                    .map(UserResponseDTO::fromEntity)
+                    .orElseThrow(() -> new NoSuchElementException("UserId 에 해당하는 유저가 없습니다."));
+        if(email!=null)
+            return userRepository.findByEmail(email)
+                    .map(UserResponseDTO::fromEntity)
+                    .orElseThrow(() -> new NoSuchElementException("Email 에 해당하는 유저가 없습니다."));
+
+        throw new IllegalArgumentException("입력 조건을 정확히 입력해주세요.");
     }
 
     @Override
     public UserResponseDTO getUserById(Long id) {
-        UserResponseDTO res =  userRepository.findById(id).map(UserResponseDTO::fromEntity).orElse(null);
-        return null;
+        return userRepository.findById(id)
+                .map(UserResponseDTO::fromEntity)
+                .orElseThrow(() -> new NoSuchElementException("ID 값에 해당하는 유저가 없습니다."));
     }
 
     @Override
-    public UserResponseDTO updateUser(UpdateUserRequestDTO req) {
+    @Transactional
+    public UserResponseDTO updateUser(Long id, UpdateUserRequestDTO req) {
+
+        //TODO 로그인 검증 과정 생략
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not Found"));
+
+
+
         return null;
     }
+
 
     @Override
     public void deleteUser(Long id) {
