11ee74a5dd6b9219e432ba0a3d413c89d7861e4e
jhjang <jhjang@naver.com>
2025-11-07 17:12:39 +0900

    feat : Security 에서 사용하는 UserDetail 과 GrantedAuthority 를 상속받아 사용하도록 수정

--- src/main/java/com/example/chordjang/user/User.java
+++ src/main/java/com/example/chordjang/user/User.java
@@ -3,12 +3,21 @@ package com.example.chordjang.user;
 import com.example.chordjang.util.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Entity
 @Getter
 @Table(name = "users")
-@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA는 기본 생성자를 필요로 합니다. 무분별한 생성을 막기 위해 protected로 설정합니다.
-public class User extends BaseEntity {
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA 는 기본 생성자를 필요로 합니다. 무분별한 생성을 막기 위해 protected로 설정합니다.
+public class User extends BaseEntity implements UserDetails {
+    // Security 에서 UserDetails Interface 객체를 사용. 필수메서드 구현 필요.
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,5 +45,36 @@ public class User extends BaseEntity {
     public void updateUser(String newEmail){
         if(newEmail != null)
             this.email =  newEmail;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(this.role.getKey()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userId; // Spring Security에서는 username을 userId로 사용
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; // 계정 만료 여부 (true: 만료되지 않음)
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true; // 계정 잠금 여부 (true: 잠기지 않음)
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; // 비밀번호 만료 여부 (true: 만료되지 않음)
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true; // 계정 활성화 여부 (true: 활성화됨)
     }
 }
