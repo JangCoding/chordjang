f1fe0574492c8b85ed3e6d5f2cad0831ad27c215
jhjang <jhjang@naver.com>
2025-11-10 16:41:26 +0900

    feat : 예외처리 추가, Swagger, Cors Config 설정

--- src/main/java/com/example/chordjang/config/SwaggerConfig.java
+++ src/main/java/com/example/chordjang/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.example.chordjang.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +12,23 @@ import org.springframework.context.annotation.Configuration;
 class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("ChordJang API Document")
+                .version("v0.0.1")
+                .description("ChordJang 프로젝트의 API 명세서입니다.");
+
+        String jwtSchemeName = "JWT Authorization";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 토큰 형식 지정
+
         return new OpenAPI()
-                .components(new Components())
-                .info(new Info()
-                        .title("ChordJang API")
-                        .description("ChordJang API schema")
-                        .version("1.0.0"));
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
