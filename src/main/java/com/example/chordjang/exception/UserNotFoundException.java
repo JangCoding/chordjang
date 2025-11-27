a06637ab551772951596aaf01c0f052a626ea095
jhjang <jhjang@naver.com>
2025-11-28 08:53:09 +0900

    feat : MySQL 설정, Transaction 설정

--- src/main/java/com/example/chordjang/exception/UserNotFoundException.java
+++ src/main/java/com/example/chordjang/exception/UserNotFoundException.java
@@ -7,7 +7,7 @@ public class UserNotFoundException extends RuntimeException {
     private final ErrorCodeEnum errorCode;
 
     public UserNotFoundException(ErrorCodeEnum errorCode, String by, Object value) {
-        super(String.format("%s. (%s: %s)", errorCode.getMessage(), by, value));
+        super(String.format("%s (%s: %s)", errorCode.getMessage(), by, value));
         this.errorCode = errorCode;
     }
 }
