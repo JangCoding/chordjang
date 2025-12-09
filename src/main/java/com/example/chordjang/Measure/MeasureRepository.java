c4e1d1c3401a07b6e14621f423cd5a1b174bc7e6
jhjang <jhjang@naver.com>
2025-12-09 17:12:43 +0900

    feat : Measure 는 MusicSheet 통해 제어하도록 위임. request 통합.

--- src/main/java/com/example/chordjang/Measure/MeasureRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.chordjang.Measure;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MeasureRepository extends JpaRepository<Measure, Long> {
-}
--- src/main/java/com/example/chordjang/Measure/MeasureService.java
+++
@@ -1,7 +0,0 @@
-package com.example.chordjang.Measure;
-
-import java.util.List;
-
-public interface MeasureService {
-    List<Measure> getMeasures(Long musicSheetId);
-}
